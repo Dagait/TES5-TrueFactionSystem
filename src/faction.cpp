0a5cad727adab8d10f50181d895bd6e0b062d86e
Dagait <dagait01@hs-esslingen.de>
2024-09-28 18:07:39 +0200

    Added more factions

--- src/faction.cpp
+++ src/faction.cpp
@@ -1,9 +1,24 @@
 #include "faction.h"
 
 const std::vector<std::pair<std::string, RE::FormID>> factionArmorTags = {
-    {"npeBanditFaction", 0x0001BCC0},      // Bandit faction
-    {"npeImperialFaction", 0x0002BF9A},    // Imperial faction
-    {"npeStormcloakFaction", 0x00028849},  // Stormcloak faction
+    {"npeBanditFaction", 0x0001BCC0},
+    {"npeImperialFaction", 0x0002BF9A}, 
+    {"npeBladesFaction", 0x00072834},
+    {"npeCompanionsFaction", 0x00048362}, 
+    {"npeDawnguardFaction", 0x00014217}, 
+    {"npeDawnstarFaction", 0x0002816E},
+    {"npeFalkreathFaction", 0x00028170},  
+    {"npeForswornFaction", 0x00043599},  
+    {"npeMarkarthFaction", 0x0002816C},
+    {"npeMorthalFaction", 0x0002816D},    
+    {"npeNightingaleFaction", 0x000DEED6}, 
+    {"npeRiftenFaction", 0x0002816B},
+    {"npeSolitudeFaction", 0x00029DB0},  
+    {"npeSolstheimFaction", 0x00018279},   
+    {"npeStormcloaksFaction", 0x00028849},
+    {"npeWhiterunFaction", 0x000267EA},   
+    {"npeWindhelmFaction", 0x000267E3},      
+    {"npeWinterholdFaction", 0x00014217}
 };
 
 std::vector<std::pair<std::string, RE::TESFaction *>> GetRelevantFactions() {
@@ -26,6 +41,81 @@ std::vector<std::pair<std::string, RE::TESFaction *>> GetRelevantFactions() {
     RE::TESFaction *stormcloakFaction = RE::TESForm::LookupByID<RE::TESFaction>(0x00028849);
     if (stormcloakFaction) {
         factions.push_back({"StormcloakFaction", stormcloakFaction});
+    }
+
+    RE::TESFaction *bladesFaction = RE::TESForm::LookupByID<RE::TESFaction>(0x00072834);
+    if (bladesFaction) {
+        factions.push_back({"BladesFaction", bladesFaction});
+    }
+
+    RE::TESFaction *companionsFaction = RE::TESForm::LookupByID<RE::TESFaction>(0x00048362);
+    if (companionsFaction) {
+        factions.push_back({"CompanionsFaction", companionsFaction});
+    }
+
+    RE::TESFaction *dawnguardFaction = RE::TESForm::LookupByID<RE::TESFaction>(0x00014217);
+    if (dawnguardFaction) {
+        factions.push_back({"DawnguardFaction", dawnguardFaction});
+    }
+
+    RE::TESFaction *dawnstarFaction = RE::TESForm::LookupByID<RE::TESFaction>(0x0002816E);
+    if (dawnstarFaction) {
+        factions.push_back({"DawnstarFaction", dawnstarFaction});
+    }
+
+    RE::TESFaction *falkreathFaction = RE::TESForm::LookupByID<RE::TESFaction>(0x00028170);
+    if (falkreathFaction) {
+        factions.push_back({"FalkreathFaction", falkreathFaction});
+    }
+
+    RE::TESFaction *forswornFaction = RE::TESForm::LookupByID<RE::TESFaction>(0x00043599);
+    if (forswornFaction) {
+        factions.push_back({"ForswornFaction", forswornFaction});
+    }
+
+    RE::TESFaction *markarthFaction = RE::TESForm::LookupByID<RE::TESFaction>(0x0002816C);
+    if (markarthFaction) {
+        factions.push_back({"MarkarthFaction", markarthFaction});
+    }
+
+    RE::TESFaction *morthalFaction = RE::TESForm::LookupByID<RE::TESFaction>(0x0002816D);
+    if (morthalFaction) {
+        factions.push_back({"MorthalFaction", morthalFaction});
+    }
+
+    RE::TESFaction *nightingaleFaction = RE::TESForm::LookupByID<RE::TESFaction>(0x000DEED6);
+    if (nightingaleFaction) {
+        factions.push_back({"NightingaleFaction", nightingaleFaction});
+    }
+
+    RE::TESFaction *riftenFaction = RE::TESForm::LookupByID<RE::TESFaction>(0x0002816B);
+    if (riftenFaction) {
+        factions.push_back({"RiftenFaction", riftenFaction});
+    }
+
+    RE::TESFaction *solitudeFaction = RE::TESForm::LookupByID<RE::TESFaction>(0x00029DB0);
+    if (solitudeFaction) {
+        factions.push_back({"SolitudeFaction", solitudeFaction});
+    }
+
+    RE::TESFaction *solstheimFaction = RE::TESForm::LookupByID<RE::TESFaction>(0x00018279);
+    if (solstheimFaction) {
+        factions.push_back({"SolstheimFaction", solstheimFaction});
+    }
+
+    RE::TESFaction *whiterunFaction = RE::TESForm::LookupByID<RE::TESFaction>(0x000267EA);
+    if (whiterunFaction) {
+        factions.push_back({"WhiterunFaction", whiterunFaction});
+    }
+
+    RE::TESFaction *windhelmFaction = RE::TESForm::LookupByID<RE::TESFaction>(0x000267E3);
+    if (windhelmFaction) {
+        factions.push_back({"WindhelmFaction", windhelmFaction});
+    }
+
+    RE::TESFaction *winterholdFaction = RE::TESForm::LookupByID<RE::TESFaction>(0x00014217);
+    if (winterholdFaction) {
+        factions.push_back({"WinterholdFaction", winterholdFaction});
     }
 
     return factions;
