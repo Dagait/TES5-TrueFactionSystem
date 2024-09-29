6c4e4552d6b9a1237b369ab511f27ccd01cebcf5
NullPointerExcy <dagait01@hs-esslingen.de>
2024-09-29 18:33:33 +0200

    Factions update

--- src/faction.cpp
+++ src/faction.cpp
@@ -4,8 +4,7 @@ const std::vector<std::pair<std::string, RE::FormID>> factionArmorTags = {
     {"npeBanditFaction", 0x0001BCC0},
     {"npeImperialFaction", 0x0002BF9A}, 
     {"npeBladesFaction", 0x00072834},
-    {"npeCompanionsFaction", 0x00048362}, 
-    {"npeDawnguardFaction", 0x00014217}, 
+    {"npeCompanionsFaction", 0x00048362},
     {"npeDawnstarFaction", 0x0002816E},
     {"npeFalkreathFaction", 0x00028170},  
     {"npeForswornFaction", 0x00043599},  
@@ -13,22 +12,21 @@ const std::vector<std::pair<std::string, RE::FormID>> factionArmorTags = {
     {"npeMorthalFaction", 0x0002816D},    
     {"npeNightingaleFaction", 0x000DEED6}, 
     {"npeRiftenFaction", 0x0002816B},
-    {"npeSolitudeFaction", 0x00029DB0},  
-    {"npeSolstheimFaction", 0x00018279},   
+    {"npeSolitudeFaction", 0x00029DB0},
     {"npeStormcloaksFaction", 0x00028849},
     {"npeWhiterunFaction", 0x000267EA},   
     {"npeWindhelmFaction", 0x000267E3},      
-    {"npeWinterholdFaction", 0x00014217}
-};
+    {"npeWinterholdFaction", 0x00014217},
+    {"npeThalmorFaction", 0x00039F26}};
 
 static std::unordered_map<RE::FormID, std::string> factionFormIDToTagMap = {
-    {0x0001BCC0, "npeBanditFaction"},
-    {0x0002BF9A, "npeImperialFaction"},    {0x00072834, "npeBladesFaction"},
-    {0x00048362, "npeCompanionsFaction"}, {0x00014217, "npeDawnguardFaction"},   {0x0002816E, "npeDawnstarFaction"},
+    {0x0001BCC0, "npeBanditFaction"},     {0x0002BF9A, "npeImperialFaction"},    {0x00072834, "npeBladesFaction"},
+    {0x00048362, "npeCompanionsFaction"},   {0x0002816E, "npeDawnstarFaction"},
     {0x00028170, "npeFalkreathFaction"},  {0x00043599, "npeForswornFaction"},    {0x0002816C, "npeMarkarthFaction"},
     {0x0002816D, "npeMorthalFaction"},    {0x000DEED6, "npeNightingaleFaction"}, {0x0002816B, "npeRiftenFaction"},
-    {0x00029DB0, "npeSolitudeFaction"},   {0x00018279, "npeSolstheimFaction"},   {0x00028849, "npeStormcloaksFaction"},
-    {0x000267EA, "npeWhiterunFaction"},   {0x000267E3, "npeWindhelmFaction"},    {0x00014217, "npeWinterholdFaction"}};
+    {0x00029DB0, "npeSolitudeFaction"},   {0x00028849, "npeStormcloaksFaction"},
+    {0x000267EA, "npeWhiterunFaction"},   {0x000267E3, "npeWindhelmFaction"},    {0x00014217, "npeWinterholdFaction"},
+    {0x00039F26, "npeThalmorFaction"}};
 
 const std::vector<RE::BGSBipedObjectForm::BipedObjectSlot> allArmorSlots = {
     RE::BGSBipedObjectForm::BipedObjectSlot::kHead,     RE::BGSBipedObjectForm::BipedObjectSlot::kBody,
@@ -62,11 +60,6 @@ std::vector<std::pair<std::string, RE::TESFaction *>> GetRelevantFactions() {
     RE::TESFaction *companionsFaction = RE::TESForm::LookupByID<RE::TESFaction>(0x00048362);
     if (companionsFaction) {
         factions.push_back({"CompanionsFaction", companionsFaction});
-    }
-
-    RE::TESFaction *dawnguardFaction = RE::TESForm::LookupByID<RE::TESFaction>(0x00014217);
-    if (dawnguardFaction) {
-        factions.push_back({"DawnguardFaction", dawnguardFaction});
     }
 
     RE::TESFaction *dawnstarFaction = RE::TESForm::LookupByID<RE::TESFaction>(0x0002816E);
@@ -109,11 +102,6 @@ std::vector<std::pair<std::string, RE::TESFaction *>> GetRelevantFactions() {
         factions.push_back({"SolitudeFaction", solitudeFaction});
     }
 
-    RE::TESFaction *solstheimFaction = RE::TESForm::LookupByID<RE::TESFaction>(0x00018279);
-    if (solstheimFaction) {
-        factions.push_back({"SolstheimFaction", solstheimFaction});
-    }
-
     RE::TESFaction *whiterunFaction = RE::TESForm::LookupByID<RE::TESFaction>(0x000267EA);
     if (whiterunFaction) {
         factions.push_back({"WhiterunFaction", whiterunFaction});
@@ -127,6 +115,11 @@ std::vector<std::pair<std::string, RE::TESFaction *>> GetRelevantFactions() {
     RE::TESFaction *winterholdFaction = RE::TESForm::LookupByID<RE::TESFaction>(0x00014217);
     if (winterholdFaction) {
         factions.push_back({"WinterholdFaction", winterholdFaction});
+    }
+
+    RE::TESFaction *thalmorFaction = RE::TESForm::LookupByID<RE::TESFaction>(0x00039F26);
+    if (thalmorFaction) {
+        factions.push_back({"ThalmorFaction", thalmorFaction});
     }
 
     return factions;
